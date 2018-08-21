79dbb1b3e548eb4ab71010ac9efc1aae6da50d03
leonardoCavazzani <cavazzanileonardo@gmail.com>
2018-08-21 15:28:43 -0500

    first action test

--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions';
+import { bindActionCreators } from 'redux'
 
 class BookList extends Component{
     
     renderList(){
         return this.props.books.map((book) => {
             return(
-            <li key={book.title} className="list-group-item">{book.title}</li>
+            <li key={book.title}
+             className="list-group-item"
+             onClick={() => this.props.selectBook(book)}>{book.title}</li>
             )
         })
     }
@@ -20,9 +24,15 @@ class BookList extends Component{
 }
 
 function mapStateToProps(state){
+    //transform the application state to props
     return {
         books: state.books,
     }
 }
 
-export default connect(mapStateToProps) (BookList);
+function mapDispatchToProps(dispatch){
+    //whatever selectBook is called, the result should be passes to all reducers
+    return bindActionCreators({ selectBook: selectBook } , dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
