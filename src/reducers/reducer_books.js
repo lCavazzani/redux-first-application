3aebe64b545be203b6e19f866b254ca214a5bbc3
leonardoCavazzani <cavazzanileonardo@gmail.com>
2018-08-21 14:03:14 -0500

    showing list of books with redux

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        {title: 'Harry potter'},
+        {title: 'Dark Tower'},
+        {title: 'Lord of the Rings'},
+        {title: 'React Guide'},
+    ]
+}
